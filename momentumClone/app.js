2057b226be1909ffe341df1c8b07727340504566
Jgone2 <jgoneit@gmail.com>
2022-01-17 18:33:04 +0900

    localStorage Login

--- momentumClone/app.js
+++ momentumClone/app.js
@@ -6,46 +6,52 @@
 
 // #login-form Element를 가져오는 방법2
 // const loginInput = document.querySelector("#login-form input");
-// const loginButton = document.querySelector("#login-form button");
+// const loginBtn = document.querySelector("#login-form button");
+const HIDDEN_CLASS = "hidden";
+const USERNAME_KEY = "username"
 
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 // const link = document.querySelector("a");
 
-function onLoginBtnClick() {
-  const userName = loginInput.value;
-  // form이 submit이 되고 있기 때문에 페이지가 새로 고침이 됨
-  console.log("hello " + userName);
-  // if(username ==="") {
-  //   alert("please write your name");
-  // } else if(username.length > 15) {
-  //   alert("Your name is too long");
-  // } else {
-  //   alert("Welcome to here");
-  // }
-  // form을 활용한 유효성 검사를 html에서 실행하면 js에서는 위의 코드를 사용할 필요가 없음
-}
+// function onLoginBtnClick() {
+//   const userName = loginInput.value;
+//   // form이 submit이 되고 있기 때문에 페이지가 새로 고침이 됨
+//   console.log("hello " + userName);
+//   // if(username ==="") {
+//   //   alert("please write your name");
+//   // } else if(username.length > 15) {
+//   //   alert("Your name is too long");
+//   // } else {
+//   //   alert("Welcome to here");
+//   // }
+//   // form을 활용한 유효성 검사를 html에서 실행하면 js에서는 위의 코드를 사용할 필요가 없음
+// }
 
-const HIDDEN_CLASS = "hidden";
+function paintGreetings(username) {
+  greeting.innerHTML = `Hello ${username}`
+  greeting.classList.remove(HIDDEN_CLASS);
+}
 
 function onLoginSubmit(event) {
   event.preventDefault(); // 기본동작 실행 제어
   loginForm.classList.add(HIDDEN_CLASS);
+  loginBtn.innerHTML = `Log Out`;
   const username = loginInput.value;
-  greeting.innerHTML = `Hello ${username}`;
-  greeting.classList.remove(HIDDEN_CLASS);
-  // console.log(event);
-  // console.log(loginInput.value);
-  console.log(username);
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
+  // console.log(username);
 }
 
-// function goLink(event) {
-//   event.preventDefault();
-//   console.log(event);
-//   console.dir(event);
-// }
-
-loginForm.addEventListener("submit", onLoginSubmit);
-// link.addEventListener("click", goLink);
+// Algorithm
+if(savedUsername === null) {
+  // show the form
+  loginForm.classList.remove(HIDDEN_CLASS);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  // show the greetings
+  paintGreetings(savedUsername);
+}
