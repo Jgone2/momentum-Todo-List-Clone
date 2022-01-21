a50f8fb373b2b59ceae142aeb09ef57a678fce3b
Jgone2 <jgoneit@gmail.com>
2022-01-21 21:01:54 +0900

    todolist func Finish

--- momentumClone/js/todo.js
+++ momentumClone/js/todo.js
@@ -12,13 +12,17 @@ function saveTodoArr() {
 function deleteTodo(event) {
   // const li = event.target.parentElement;
   const li = this.parentElement;
+  const listID = parseInt(li.id);
   li.remove();
+  todoArr = todoArr.filter((todo) => todo.id !== listID);
+  saveTodoArr();
 }
 
 function paintTodo(newTodo) {
   const li = document.createElement("li");
+  li.id = newTodo.id;
   const span = document.createElement("span");
-  span.innerText = newTodo;
+  span.innerText = newTodo.text;
   const btn = document.createElement("button");
   btn.innerText = "✖️";
   btn.addEventListener("click", deleteTodo);
@@ -31,8 +35,12 @@ function todoSubmit(event) {
   event.preventDefault();
   const newTodo = todoInput.value;
   todoInput.value = "";
-  todoArr.push(newTodo);
-  paintTodo(newTodo);
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  todoArr.push(newTodoObj);
+  paintTodo(newTodoObj);
   saveTodoArr();
 }
 
