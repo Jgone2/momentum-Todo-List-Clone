82b4c7521cd64c84128915df2d49587f1b68bf8a
Jgone2 <jgoneit@gmail.com>
2022-01-21 20:25:34 +0900

    localStorage를 사용한 List save

--- momentumClone/js/todo.js
+++ momentumClone/js/todo.js
@@ -2,20 +2,46 @@ const todoForm = document.querySelector("#todo-form");
 const todoInput = todoForm.querySelector("input");
 const todoList = document.querySelector("#todo-list");
 
+const TODOS_KEY = "todoArr"
+let todoArr = [];
+
+function saveTodoArr() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(todoArr));
+}
+
+function deleteTodo(event) {
+  // const li = event.target.parentElement;
+  const li = this.parentElement;
+  li.remove();
+}
+
 function paintTodo(newTodo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
-  li.appendChild(span);
   span.innerText = newTodo;
+  const btn = document.createElement("button");
+  btn.innerText = "✖️";
+  btn.addEventListener("click", deleteTodo);
+  li.appendChild(span);
+  li.appendChild(btn);
   todoList.appendChild(li);
-
 }
 
 function todoSubmit(event) {
   event.preventDefault();
   const newTodo = todoInput.value;
   todoInput.value = "";
+  todoArr.push(newTodo);
   paintTodo(newTodo);
+  saveTodoArr();
 }
 
 todoForm.addEventListener("submit", todoSubmit);
+
+const savedTodoArr = localStorage.getItem(TODOS_KEY);
+
+if(savedTodoArr !== null) {
+  const parsedToDoArr = JSON.parse(savedTodoArr);
+  todoArr = parsedToDoArr;
+  parsedToDoArr.forEach(paintTodo);
+}
